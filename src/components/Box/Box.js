a9647a19a2010a4e8402d0efb9b30b2d6b6283fb
Suraj Rao <suraj_rao@berkeley.edu>
2023-09-27 18:49:45 -0700

    test2

---
+++ src/components/Box/Box.js
@@ -0,0 +1,12 @@
+
+import './Box.css'
+
+const BlueBox = ({name}) => {
+    return (
+        <div className="blue-box">
+            <h1>{name}</h1>
+        </div>
+    )
+}
+
+export default BlueBox;
